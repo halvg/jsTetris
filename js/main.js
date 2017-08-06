9fbaff9b41e527ce2ed6060f3726eb19cb1fdabe
Héctor Álvarez <norealemail@email.com>
2017-08-06 12:00:15 +0200

    Try different approach

--- js/main.js
+++ js/main.js
@@ -1,70 +1,81 @@
 
 
-initGame();
-
-////////////////////////////////////////////////////////////////////////////////
-
-var canvas = document.getElementById("tetrisCanvas");
-var canvW = canvas.width;
-var canvH = canvas.height;
-var ctx = canvas.getContext("2d");
-
-var tetrisBoard = new TetrisBoard({cols: 12, rows: 18, canvW: canvW, canvH: canvH});
-
-function initGame() {
-  gameLoop();
-}
+var tBoard = new TBoard();
 
 
-var fps = 1/10;
-var last = new Date().getTime();
-function gameLoop() {
-  var now = new Date().getTime();
-  var delta = (now - last)/1000;
-  if (delta >= fps) {
-    console.log(delta);
-    updateGame();
-    renderGame();
-    last = new Date().getTime();
-  }
-  //requestAnimationFrame(gameLoop);
-}
+function TBoard() {
+  this.boardNumColumns = 10;
+  this.boardNumRows = 16;
+  this.board = [];
 
+  this.activeTetro = null;
 
-function updateGame() {
-
-}
-
-function renderGame() {
-}
-
-
-function TetrisBoard(boardConf) {
-  this.cols = boardConf.cols;
-  this.rows = boardConf.rows;
-  this.canvW = boardConf.canvW;
-  this.canvH = boardConf.canvH;
-
-  this.cells = [];
-
-  //create cells
-  var cWidth = this.canvW / this.cols;
-  var cHeight = this.canvH / this.rows;
-  for (var i = 0; i < this.cols; i++) {
-    var col = [];
-    for (var j = 0; j < this.rows; j++) {
-      var cell = new BoardCell({x: i*cWidth, y: j*cHeight}, cWidth, cHeight);
-      col[j] = cell;
+  //-- Construction
+  for (var i = 0; i < this.boardNumRows; i++) {
+    var row = [];
+    for (var j = 0; j < this.boardNumColumns; j++) {
+      row.push(0);
     }
-    this.cells[i] = col;
+    this.board.push(row);
+  }
+  //--
+
+  this.pushTetro = function(tetroMatrix) {
+    this.activeTetro = {};
+    this.activeTetro.offset = {y:0, x:3} // y for row offset, x for column offset
+    this.activeTetro.matrix = tetroMatrix;
+  }
+
+  this.textPrintGState = function() {
+    var text = "";
+    for (var i = 0; i < this.board.length; i++) {
+      for (var j = 0; j < this.board[0].length; j++) { // iterate columns
+        var yLength = this.activeTetro.matrix.length;
+        var yInit = this.activeTetro.offset.y;
+        var yEnd = (yInit + yLength) - 1;
+        var xLength = this.activeTetro.matrix[0].length;
+        var xInit = this.activeTetro.offset.x;
+        var xEnd = (xInit + xLength) - 1;
+        if ((i >= yInit && i <= yEnd) && (j >= xInit && j <= xEnd)) {
+          text += "9" + "  ";
+        } else {
+          text += this.board[i][j] + "  ";
+        }
+      }
+      text += "\n";
+    }
+    return text;
   }
 
 }
 
-function BoardCell(pos, width, height) {
-  this.pos = pos;
-  this.width = width;
-  this.heigth = height;
+var tTetro = [
+  [1, 1, 1],
+  [0, 1, 0],
+  [0, 0, 0]
+];
 
-  console.log("Cell: " + pos.x + " " + pos.y);
+
+
+// util functions
+function textPrint2DMatrix(matrix) {
+  var text = "";
+  for (var i = 0; i < matrix.length; i++) { // iterate rows
+    for (var j = 0; j < matrix[0].length; j++) { // iterate columns
+      text += matrix[i][j] + "  ";
+    }
+    text += "\n";
+  }
+  return text;
+}
+
+document.addEventListener("keydown", doKeyDown);
+
+function doKeyDown(event) {
+  if (event.keyCode == 40) { //ArrowDown
+    if (tBoard.activeTetro != null) {
+      tBoard.activeTetro.offset.y++;
+    }
+  }
+
 }
